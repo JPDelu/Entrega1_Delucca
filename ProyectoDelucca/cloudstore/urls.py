5ddc0ef2982a7640d966428e3ac16f0ce3c1f38f
JPDelucca <juan.delucca21@gmail.com>
2022-11-23 01:12:43 -0300

    Se corrigen ultimos detalles y se escribe informacion en README

--- ProyectoDelucca/cloudstore/urls.py
+++ ProyectoDelucca/cloudstore/urls.py
@@ -3,9 +3,9 @@ from cloudstore.views import *
 
 urlpatterns = [
     path('', inicio, name="Inicio"),
-    path('consolas', consolas, name="Consolas"),
-    path('juegos', juegos, name="Juegos"),
-    path('accesorios', accesorios, name="Accesorios"),
+    path('consolas/', consolas, name="Consolas"),
+    path('juegos/', juegos, name="Juegos"),
+    path('accesorios/', accesorios, name="Accesorios"),
     path('crear_juegos/', crear_juegos, name="Form_juegos"),
     path('crear_consolas/', creacion_consolas, name="Form_consolas"),
     path('busqueda_consolas/', buscar_consola, name="Buscador_consolas"),
